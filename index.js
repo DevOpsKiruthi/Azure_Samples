431abfb0eda7a43ec15e2793f951b608172b0f40
DevOpsKiruthi <102915675+DevOpsKiruthi@users.noreply.github.com>
2023-11-22 16:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const db = {
       description: `Test account`,
       balance: 75,
       transactions: [
-        { id: '1', date: '2020-10-01', object: 'Pocket money', amount: 50 },
+        { id: '1', date: '2020-10-01', object: 'Pocket money', amount: 100 },
         { id: '2', date: '2020-10-03', object: 'Book', amount: -10 },
         { id: '3', date: '2020-10-04', object: 'Sandwich', amount: -5 }
       ],
